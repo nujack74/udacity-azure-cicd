75853712b2cd8c0a194509b54c922a6ea8175b3f
Sascha Steinert <steinert@germanynet.de>
2022-05-04 11:33:37 +0000

    commands.sh corrected and .azure added in .gitignore

--- commands.sh
+++ commands.sh
@@ -3,7 +3,7 @@
 git clone git@github.com:nujack74/udacity-azure-cicd.git
 cd udacity-azure-cicd
 make all
-az webapp up -n udacity-flask-ml-service -l eastus --sku F1
+az webapp up -n udacity-flask-ml-service -l eastus --sku F1 -g steinert_rg_6326
 ./make_predict_azure_app.sh
 az webapp log tail -n udacity-flask-ml-service -g steinert_rg_6326
-az group delete -n steinert_rg_6326
+az group delete -g steinert_rg_6326
